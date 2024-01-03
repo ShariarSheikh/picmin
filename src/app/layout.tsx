c13e2a7c17a139041d90978562446a5ee98e5f71
ShariarSheikh <shariar.dev@gmail.com>
2024-01-03 18:43:26 +0600

    added meta(for Content-Security-Policy) in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,10 @@ export default function RootLayout({
           href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap'
           crossOrigin='anonymous'
         />
+        <meta
+          httpEquiv='Content-Security-Policy'
+          content='upgrade-insecure-requests'
+        />
       </head>
       <body>
         <Provider>
--- src/app/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function middleware(_request: NextRequest) {
-  return NextResponse.next()
-}
